8ceaa809d98eb98549f62f1f7c1424c513853d47
Michael Thomas <mbthomas@gmail.com>
2015-06-25 11:42:18 -0400

    Update application.py

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods=('GET',))
 def index():
     return render_template('index.html',
-                           title=os.environ['PAGE_TITLE'])
+                           title="Welcome!")
 
 
 if __name__ == '__main__':
